ed11e298a4221f49034bcc54d8e455eef2e8ac63
Builder <builder@android.com>
2013-09-21 10:11:44 -0400

    fix typos which prevent overlay from working
    Change-Id: I052b219782c94b119ce0f044e47988dffa9956ad

--- aetherar_jfltexx.mk
+++ aetherar_jfltexx.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/samsung/jfltexx/cm.mk)
 
 # aetherar vendor
-$(call inherit-product-if-exsists, vendor/aetherar/config/common.mk)
+$(call inherit-product-if-exists, vendor/aetherar/config/common.mk)
 
 CM_BUILD := jfltexx-aetherar
 
