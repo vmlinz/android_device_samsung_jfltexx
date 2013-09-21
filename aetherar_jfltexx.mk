9861b2708f41bf145b73dd8d72ff74d4f533bf4a
Builder <builder@android.com>
2013-09-21 05:43:40 -0400

    inherit vendor aetherar if it exists
    Change-Id: I573435ecff412391c509e00fcdbf3b6af4ad94d4

--- aetherar_jfltexx.mk
+++ aetherar_jfltexx.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/samsung/jfltexx/cm.mk)
 
 # aetherar vendor
-$(call inherit-product, vendor/aetherar/config/common.mk)
+$(call inherit-product-if-exsists, vendor/aetherar/config/common.mk)
 
 PRODUCT_DEVICE := jfltexx
 PRODUCT_NAME := aetherar_jfltexx
