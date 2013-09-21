63e0d430b3c0108a14322ed785ad0a970e9f1737
Builder <builder@android.com>
2013-09-21 04:42:06 -0400

    add aetherar vendor product on top of cm product
    Change-Id: Iae864822a959aff6b41a2736b01d11fde994c556

---
+++ aetherar_jfltexx.mk
@@ -0,0 +1,9 @@
+# cm product
+$(call inherit-product, device/samsung/jfltexx/cm.mk)
+
+# aetherar vendor
+$(call inherit-product, vendor/aetherar/config/common.mk)
+
+PRODUCT_DEVICE := jfltexx
+PRODUCT_NAME := aetherar_jfltexx
+
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,3 @@
 add_lunch_combo cm_jfltexx-eng
+add_lunch_combo aetherar_jfltexx-eng
 
